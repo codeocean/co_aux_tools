f6f21223387e6e9a74e7e88ea6d623e17c1a1684
zappuf <frank@codeocean.com>
2023-03-02 22:26:43 -0500

    fix import error

--- src/codeoceanauxtools/get_rev_file.py
+++ src/codeoceanauxtools/get_rev_file.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import sys
 
-from .get_read_pattern import get_read_pattern
+import get_read_pattern
 
 
 def get_rev_file(fwd_file):
     return fwd_file.replace(
-        get_read_pattern(fwd_file, "1"), get_read_pattern(fwd_file, "2")
+        get_read_pattern.get_read_pattern(fwd_file, "1"),
+        get_read_pattern.get_read_pattern(fwd_file, "2"),
     )
 
 
