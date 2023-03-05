55b7c73d11024a856fdb14e28149e1700b8fcb80
frank <frank.zappulla@jax.org>
2023-03-04 21:38:28 -0500

    date and time format for logger

--- src/codeoceanauxtools/get_logger.py
+++ src/codeoceanauxtools/get_logger.py
@@ -87,7 +87,7 @@ def generate_logger(
     file_handle = logging.FileHandler(f"{LOGGER_DIR_PATH}/{name}.log")
     file_handle.setLevel(logging_level)
     # create formatter and add it to the handlers
-    log_format = logging.Formatter(format_string)
+    log_format = logging.Formatter(format_string, "%Y-%m-%d %H:%M:%S")
     file_handle.setFormatter(log_format)
 
     logger.addHandler(file_handle)
